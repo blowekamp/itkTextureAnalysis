b3911a8ab891859c50c7e3c8819d1a9fa0eaf4af
Bradley Lowekamp <blowekamp@mail.nih.gov>
2015-03-02 13:10:13 -0500

    Excluding module from being enabled by default

--- itk-module.cmake
+++ itk-module.cmake
@@ -4,6 +4,12 @@
 get_filename_component( MY_CURENT_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 file( READ "${MY_CURENT_DIR}/README" DOCUMENTATION )
 
+# ITK version 4.5 changed it from EXCLUDE_FROM_ALL to EXCLUDE_FROM_DEFAULT
+set( _EXCLUDE "EXCLUDE_FROM_ALL" )
+if (NOT "${ITK_VERSION_MAJOR}.${ITK_VERSION_MINOR}.${ITK_VERSION_MINOR_PATCH}" VERSION_LESS "4.5")
+  set( _EXCLUDE "EXCLUDE_FROM_DEFAULT" )
+endif()
+
 # define the dependencies of the include module and the tests
 itk_module(ITKTextureAnalysis
   DEPENDS
@@ -17,4 +23,5 @@ itk_module(ITKTextureAnalysis
     ITKPNG
   DESCRIPTION
     "${DOCUMENTATION}"
+  ${_EXCLUDE}
 )
